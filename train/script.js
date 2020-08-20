2d25d55583592d5d8f710e24a70be7939678aa1e
Erwan R <rocforterwan@yahoo.fr>
2020-08-20 14:40:05 +0200

    Tchoutchou
    Le ptit train.

---
+++ train/script.js
@@ -0,0 +1,49 @@
+let nonTraction = 200;
+let position = 0;
+let animation;
+let speed = 0;
+let interTrain = setInterval(accelerer, nonTraction - speed);
+
+let train = document.getElementById("train");
+train.addEventListener("click",accelerer);
+
+let btnStopper = document.getElementById("btnStopper");
+btnStopper.addEventListener("click", stopperTrain)
+
+function accelerer(){
+
+	if(speed < 250){
+
+		speed++;
+
+	}
+
+	function frame(){
+
+		posActuelle = document.getElementById("train").style.left;
+		testerPosition(posActuelle);
+		document.getElementById("train").style.left = position++ + "px";
+
+	}
+
+	clearInterval(interTrain);
+	interTrain = setInterval(accelerer, nonTraction - speed);
+	frame();
+}
+
+function testerPosition(posActuelle){
+
+	if(parseInt(posActuelle) + 100 > 360){
+
+		clearInterval(interTrain);
+		alert("Accident.")
+
+	}
+}
+
+function stopperTrain(){
+
+	clearInterval(interTrain);
+
+}
+//et mercé pour l'inspi Joel et Guillaume
